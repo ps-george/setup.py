b65bad958c730a4541973606c361dd5ad6bcaf68
g.punter <g.punter@bboxx.co.uk>
2018-06-15 16:15:09 +0100

    Minor changes from ps-george

--- setup.py
+++ setup.py
@@ -13,22 +13,57 @@ from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = 'mypackage'
+PIPNAME = 'package' # What will be used for `pip install package`
 DESCRIPTION = 'My short description for my project.'
-URL = 'https://github.com/me/myproject'
+URL = 'https://github.com/<user>/<reponame>'
 EMAIL = 'me@example.com'
 AUTHOR = 'Awesome Soul'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = None
 
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
-
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
 # Except, perhaps the License and Trove Classifiers!
 # If you do change the License, remember to change the Trove Classifier for that!
+
+def get_requirements(remove_links=True):
+    """
+    lists the requirements to install.
+
+    [source](https://stackoverflow.com/questions/12518499/pip-ignores-dependency-links-in-setup-py#answer-38043585)
+    """
+    requirements = []
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        with open('DecoraterBotUtils.egg-info\requires.txt') as f:
+            requirements = f.read().splitlines()
+    if remove_links:
+        for requirement in requirements:
+            # git repository url.
+            if requirement.startswith("git:"):
+                requirements.remove(requirement)
+    return requirements
+
+# What packages are required for this module to be executed?
+REQUIRED = get_requirements()
+
+
+def get_links():
+    """
+    gets URL Dependency links.
+
+    [source](https://stackoverflow.com/questions/12518499/pip-ignores-dependency-links-in-setup-py#answer-38043585)
+    """
+    links_list = get_requirements(remove_links=False)
+    for link in links_list:
+        # git repository url.
+        if not link.startswith("git:"):
+            links_list.remove(link)
+    return links_list
+
+DEPENDENCY_LINKS = get_links()
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -79,30 +114,27 @@ class UploadCommand(Command):
         self.status('Pushing git tags…')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
-        
+
         sys.exit()
 
 
 # Where the magic happens:
 setup(
-    name=NAME,
+    name=PIPNAME,
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    # If you want to be able to access non-Python files from your package.
+    # package_data={'mypackage': ['example/*']},
+    # include_package_data=True,
     install_requires=REQUIRED,
-    include_package_data=True,
+    dependency_links=DEPENDENCY_LINKS,
     license='MIT',
     classifiers=[
         # Trove classifiers
@@ -118,4 +150,9 @@ setup(
     cmdclass={
         'upload': UploadCommand,
     },
+    # entry_points={
+    #     'console_scripts': [
+    #         '{} = {}.main:main'.format(PIPNAME, NAME),
+    #    ]
+    # }
 )
